ef123c51038be92715b5c18f5cd63f68bccb27b4
Iftika <iftikarahmif@gmail.com>
2021-04-26 06:58:06 +0700

    awal hari keenam

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ if (process.env.NODE_ENV !== "production")
     ...require("./config/env").options,
     dotenv: false,
   })
+  
+fastify.register(require("fastify-postgres"), require("./config/postgres"))
 fastify.register(require("fastify-static"), require("./config/static").public)
 fastify.register(require("point-of-view"), {
   engine: {
@@ -17,7 +19,7 @@ fastify.register(require("point-of-view"), {
 //declare routes
 fastify.register(require("./routes/static"))
 fastify.register(require("./routes/ssr"))
-
+fastify.register(require("./routes/api"), { prefix: "/api" })
 const start = async () => {
   try {
     await fastify.listen(process.env.PORT , "0.0.0.0")
---
+++ routes/api.js
@@ -0,0 +1,25 @@
+async function routes(fastify, options) {
+    fastify.get("/awal", async (request, reply) => {
+      reply.send({ hello: request.query.nama || "orang asing" });
+    });
+  
+      fastify.get("/table/create", async (req, reply) => {
+        const client = await fastify.pg.connect();
+        const returnVal = await client.query(
+          `CREATE TABLE ${req.query.name} (
+            id int,
+            filter varchar(255),
+            imgSrc varchar(255),
+            title varchar(255),
+            summary varchar(255),
+            galleryHref varchar(255),
+            galleryTitle varchar(255)
+        );`,
+          [req.query.name]
+        )
+        client.release()
+        return returnVal;
+      })
+    }
+    
+    module.exports = routes
